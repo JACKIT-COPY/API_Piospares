63bc38ab44f618b9b5637ce7a4d2d1da69a48a77
Jacob  Mbugua <jacobmbuguait@gmail.com>
2025-10-21 16:01:20 +0300

    New Updated main

--- api/server.js
+++
@@ -1,4 +0,0 @@
-// api/server.js
-const app = require('../app'); // path to your modified app.js
-// `app` is an express application which is also a valid request handler (req, res)
-module.exports = app;
--- config/db.js
+++ config/db.js
@@ -1,35 +1,16 @@
-// config/db.js
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 const connectDB = async () => {
-  if (cached.conn) {
-    // If already connected, reuse the connection
-    return cached.conn;
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
   }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-      // You can add other mongoose options here if needed
-    };
-
-    cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongooseInstance) => {
-      console.log('MongoDB connected');
-      return mongooseInstance;
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 };
 
 module.exports = connectDB;
